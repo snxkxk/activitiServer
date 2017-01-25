333d206af227747e9df4a10a8ac06da6d285b4b9
snxkxk <snxkxk@hotmail.com>
2017-01-25 09:05:08 +0800

    Update IdEntity.java

--- activitiServer/src/main/java/me/kafeitu/demo/activiti/entity/IdEntity.java
+++ activitiServer/src/main/java/me/kafeitu/demo/activiti/entity/IdEntity.java
@@ -6,7 +6,7 @@ import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 /**
- * 统一定义id的entity基类.A
+ * 统一定义id的entity基类.
  * <p/>
  * 基类统一定义id的属性名称、数据类型、列名映射及生成策略.
  * 子类可重载getId()函数重定义id的列名映射和生成策略.
